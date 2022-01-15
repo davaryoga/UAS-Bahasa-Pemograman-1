eb883d39818183d22d651e6ba1fa797808928861
davaryoga <92939686+davaryoga@users.noreply.github.com>
2022-01-15 22:38:20 +0700

    Add files via upload

---
+++ program1/main.py
@@ -0,0 +1,48 @@
+from view.view_nilai import *
+
+
+def menu():
+    print('=====================================')
+    print('    Pustaka Data Nilai Mahasiswa')
+    print('     Universitas Pelita Bangsa')
+    print('=====================================')
+    print('|    1. Tambah Data                 |')
+    print('|    2. Cari Data                   |')
+    print('|    3. Tampilkan Data Mahasiswa    |')
+    print('|    4. Ubah Data Mahasiswa         |')
+    print('|    5. Hapus Data Mahasiswa        |')
+    print('|    6. Selesai                     |')
+    print('=====================================')
+
+
+while True:
+    menu()
+    choose = input('Pilih Menu  : ')
+
+    if choose == '1':
+        tambah_data()
+        input('Untuk kembali ke menu utama, tekan enter')
+
+    elif choose == '2':
+        cetak_hasil_pencarian()
+        input('Untuk kembali ke menu utama, tekan enter')
+
+    elif choose == '3':
+        cetak_daftar_nilai()
+        input('Untuk kembali ke menu utama, tekan enter')
+
+    elif choose == '4':
+        ubah_data()
+        input('Untuk kembali ke menu utama, tekan enter')
+
+    elif choose == '5':
+        hapus_data()
+        input('Untuk kembali ke menu utama, tekan enter')
+
+    elif choose == '6':
+        exit()
+
+
+
+    else:
+        print("Menu yang anda maksud tidak tersedia, Silahkan pilih menu yang tersedia")
---
+++ program1/model/daftar_nilai.py
@@ -0,0 +1,38 @@
+from view.input_nilai import *
+
+dataMahasiswa = {}
+
+
+def tambah_data():
+    global dataMahasiswa
+    nama = input_nama()
+    nim = input_nim()
+    nilaiTugas = input_nilaiTugas()
+    nilaiUts = input_nilaiUts()
+    nilaiUas = input_nilaiUas()
+    nilaiAkhir = (0.30 * nilaiTugas) + (0.35 * nilaiUts) + (0.35 * nilaiUas)
+    dataMahasiswa[nama] = nim, nilaiTugas, nilaiUts, nilaiUas, nilaiAkhir
+    print("\nData Berhasil Ditambahkan!")
+    return dataMahasiswa
+
+def ubah_data():
+    nama = input("Masukkan Nama: ")
+    if nama in dataMahasiswa.keys():
+        nim = input_nim()
+        nilaiTugas = input_nilaiTugas()
+        nilaiUts = input_nilaiUts()
+        nilaiUas = input_nilaiUas()
+        nilaiAkhir = (0.30 * nilaiTugas) + (0.35 * nilaiUts) + (0.35 * nilaiUas)
+        dataMahasiswa[nama] = nim, nilaiTugas, nilaiUts, nilaiUas, nilaiAkhir
+        print("\nData Berhasil Di Update!")
+    else:
+        print("Data tidak ditemukan!")
+
+
+def hapus_data():
+    nama = input("Masukkan Nama:  ")
+    if nama in dataMahasiswa.keys():
+        del dataMahasiswa[nama]
+        print("Data",nama, "Telah dihapus!")
+    else:
+        print("Data Mahasiswa Tidak Ada".format(nama))
